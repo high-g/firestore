ae14017bda1c9d8ddff830579cdf0941c0b06549
tani <tani@1-10.com>
2021-04-26 19:39:06 +0900

    Update firebase app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+;(async () => {
+  try {
+    const firebase = require('firebase')
+    const config = {
+      apiKey: 'AIzaSyCbTmUZgrAm9V0bNoFovbRDFpWpR464Gpg',
+      authDomain: 'highgrenade-9d470.firebaseapp.com',
+      projectId: 'highgrenade-9d470',
+    }
+
+    firebase.initializeApp(config)
+    const db = firebase.firestore()
+    const userRef = db.collection('users').doc('xxx')
+    const userDoc = await userRef.get()
+    console.log(2)
+
+    if (userDoc.exists) {
+      console.log(userDoc.id)
+      console.log(userDoc.data())
+      console.log(userDoc.get('name'))
+      console.log(userDoc.get('old'))
+    } else {
+      console.log('No such document!!')
+    }
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err)}`)
+  }
+})()
